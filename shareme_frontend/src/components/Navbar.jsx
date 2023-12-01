07bfcbba59303b7ccd1500178037225c67a62276
Marlon Jesus <marlonjesus88@gmail.com>
2023-11-30 22:17:18 -0300

    starting the creation of the component structure

--- shareme_frontend/src/components/Navbar.jsx
+++ shareme_frontend/src/components/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import  { IoMdAdd, IoMdSearch } from 'react-icons/io';
 
-const Navbar = () => {
+const Navbar = ({ searchTerm, setSearchTerm, user }) => {
+  const navigate = useNavigate();
+
+  if (!user) return null;
+
   return (
-    <div>Navbar</div>
+    <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7">
+      <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
+        <IoMdSearch fontSize={21} className="" />
+      </div>
+    </div>
   )
 }
 
